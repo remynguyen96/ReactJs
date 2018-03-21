4c9de29c3099edf864402128f5ba54059acfde47
Remy Nguyen <nguyentietngocchau@gmail.com>
2018-03-21 08:30:47 +0700

    update

---
+++ part2-react/src/containers/UnitTest/HocTest/index.test.js
@@ -0,0 +1,10 @@
+import React from 'react';
+import { mount } from 'enzyme';
+import asyncComponent from './index';
+
+describe('HocTest Component', () => {
+   it('should render component', () => {
+       const FakeCompoennt = () => (<div>fake component</div>);
+       const loader = jest.fn().mockReturnValue()
+   });
+});
