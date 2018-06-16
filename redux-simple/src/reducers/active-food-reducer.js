04ffed5762f092637f79efae68d0de6315ea8ae8
Chau Nguyen <chau.nguyen@hotschedules.com>
2018-06-17 05:26:42 +0700

    Update .gitignore

--- redux-simple/src/reducers/active-food-reducer.js
+++ redux-simple/src/reducers/active-food-reducer.js
@@ -1,24 +1,24 @@
-import {REMOVE_FOOD, ADD_FOOD, SELECT_FOOD} from '../actions/actionTypes'
-import {fromJS} from 'immutable';
+import { REMOVE_FOOD, ADD_FOOD, SELECT_FOOD } from '../actions/actionTypes'
+import { fromJS } from 'immutable';
 
 
 const initialState = fromJS({
-    listData: [],
-    show: false,
+  listData: [],
+  show: false,
 });
 
 export default (state = initialState, action) => {
-    switch (action.type) {
-        case SELECT_FOOD:
-            return state
-                    .set('show', true)
-                    .set('listData', action.dataFood);
-        case REMOVE_FOOD:
-            return state.set('listData', action.idFood);
-        case ADD_FOOD:
-            return state.set('listData', action.data);
-        default:
-            return state;
-    }
+  switch (action.type) {
+    case SELECT_FOOD:
+      return state
+        .set('show', true)
+        .set('listData', action.dataFood);
+    case REMOVE_FOOD:
+      return state.set('listData', action.idFood);
+    case ADD_FOOD:
+      return state.set('listData', action.data);
+    default:
+      return state;
+  }
 }
 
