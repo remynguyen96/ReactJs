6f54eec22843ef58ecb7b06c6a302f58b8b3775e
Chau Nguyen <chau.ntn2@homecredit.vn>
2018-11-17 08:06:19 +0700

    deploy netlify

---
+++ public/assets/polyfills.ac2a785b.chunk.js
@@ -0,0 +1,2 @@
+(window.webpackJsonp=window.webpackJsonp||[]).push([[5],{643:function(n,w){}}]);
+//# sourceMappingURL=polyfills.ac2a785b.chunk.js.map
--- src/router/Spinner.js
+++
@@ -1,9 +0,0 @@
-import React from 'react';
-import Styles from './Styles.scss';
-
-export const Spinner = () => (
-  <div className={Styles.spinnerContainer}>
-    <div className={Styles.inner}></div>
-    <div className={Styles.outer}></div>
-  </div>
-);
